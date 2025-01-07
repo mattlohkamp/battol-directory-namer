c30c85ccc5739f0ab8dafa2033837ca74e624605
matt lohkamp <work@mattlohkamp.com>
2025-01-07 15:42:29 -0800

    empty input values should be empty strings

--- src/components/DirectoryNameResults.jsx
+++ src/components/DirectoryNameResults.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { selectDirectoryName } from "../state/battleDataSlice.js";
 
 export default function DirectoryNameResults() {
-	const directoryName = useSelector(selectDirectoryName);
+	const directoryName = useSelector(selectDirectoryName); //	null if battle details unavailable
 	return (
 		<label>
 			<span>Directory Name: </span>
@@ -12,7 +12,10 @@ export default function DirectoryNameResults() {
 				readOnly={true}
 				size={50}
 				placeholder="no battle data loaded..."
-				value={directoryName}
+				value={
+					directoryName ??
+					"" /* input value wants empty string instead of null */
+				}
 			/>
 		</label>
 	);
