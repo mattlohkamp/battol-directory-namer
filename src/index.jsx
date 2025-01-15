8683954a378dffa0758e7f4e88d8df03d0ecaa3d
matt lohkamp <work@mattlohkamp.com>
2025-01-15 15:57:23 -0800

    reminder about strict useeffect

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import store from "./state/store.js";
 import { Provider } from "react-redux";
 
+//	TODO: strict mode intentionally invokes useEffect twice remember
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
